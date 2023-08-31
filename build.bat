1f4b542cdecc88040ff69e0ea66ad02c9d0ae20a
MathiAs2Pique <45072138+MathiAs2Pique@users.noreply.github.com>
2023-08-31 17:27:29 +0200

    [Fix] Build for the 2 platforms

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true --self-contained true -p:DefineConstants=WIDNOWS
-dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --self-contained true -p:DefineConstants=LIUNUX
+dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true --self-contained true
+dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --self-contained true
